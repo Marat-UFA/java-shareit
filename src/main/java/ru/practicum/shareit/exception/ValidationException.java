7e9a5ad3370b82345bc8afe4ea00b638c13fc2c8
Marat <marat-konst@yandex.ru>
2022-11-12 18:53:10 +0500

    add-controllers

---
+++ src/main/java/ru/practicum/shareit/exception/ValidationException.java
@@ -0,0 +1,11 @@
+package ru.practicum.shareit.exception;
+
+public class ValidationException extends Exception {
+    public ValidationException(final String message) {
+        super(message);
+    }
+
+    public ValidationException() {
+    }
+
+}
